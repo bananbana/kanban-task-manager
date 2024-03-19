5791012fd63227f55ea38593ecbcfa60668110b6
Dawid Debosz <davelikme@gmail.com>
2024-03-19 12:35:20 +0100

    cors config

--- src/main/java/com/example/kanbantaskmanager/controllers/SubtaskController.java
+++ src/main/java/com/example/kanbantaskmanager/controllers/SubtaskController.java
@@ -1,6 +1,5 @@
 package com.example.kanbantaskmanager.controllers;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
